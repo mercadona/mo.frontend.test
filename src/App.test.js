0060f3b41f77610a908bfa525eadaa841afbe5e4
Alex Torres <torres.erades.alex@gmail.com>
2019-12-04 14:58:26 +0100

    Remove enzyme test

--- src/App.test.js
+++ src/App.test.js
@@ -1,21 +1,13 @@
 import React from 'react'
 import { render } from '@testing-library/react'
-import { shallow } from 'enzyme'
+import { shallow, mount } from 'enzyme'
 
 import App from './App'
 
-it('should show "Hi" with Testing library', async () => {
+it('should show "Hi"', async () => {
   const { getByText } = render(<App />)
 
   const text = getByText('Hi')
 
   expect(text).toBeInTheDocument()
-})
-
-it('should show "Hi" with Enzyme library', async () => {
-  const app = shallow(<App />)
-
-  const text = app.find('[role="heading"]')
-
-  expect(text.text()).toBe('Hi')
 })
