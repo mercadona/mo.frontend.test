7d3acff26099f4224624a2eb094915d7e1cf23a1
Alex Torres <torres.erades.alex@gmail.com>
2019-12-04 14:11:40 +0100

    [Added] Test example

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,21 @@
 import React from 'react'
 import { render } from '@testing-library/react'
-import { shallow, mount } from 'enzyme'
+import { shallow } from 'enzyme'
 
 import App from './App'
 
-it('should show "Hi"', async () => {
+it('should show "Hi" with Testing library', async () => {
+  const { getByText } = render(<App />)
 
+  const text = getByText('Hi')
+
+  expect(text).toBeInTheDocument()
+})
+
+it('should show "Hi" with Enzyme library', async () => {
+  const app = shallow(<App />)
+
+  const text = app.find('[role="heading"]')
+
+  expect(text.text()).toBe('Hi')
 })
