9aaa8b6fc8e964dc7cba7998029c0cb5271c8478
Mauro Gestoso <maurogestoso@gmail.com>
2021-08-16 15:22:07 +0200

    Update testing-library

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
-import React from 'react';
+import React from "react";
 
 function App() {
-  return (
-    <div role="heading">
-      Hi
-    </div>
-  );
+  return <div role="heading">Hi</div>;
 }
 
 export default App;
